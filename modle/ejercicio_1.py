74e9e974f855c684493c31e6d26e3f4d7624d4d7
JoseEduardoAA <110951654+JoseEduardoAA@users.noreply.github.com>
2022-12-06 11:53:09 -0600

    Add files via upload

---
+++ modle/ejercicio_1.py
@@ -0,0 +1,3 @@
+far=int(input("Ingresa los grados farenheit "))
+conversion=(far-32)*5/9
+print(conversion)
---
+++ modle/ejercicio_2.py
@@ -0,0 +1,7 @@
+n_1 = int(input("Ingresa un numero "))
+n_2 = int(input("Ingresa otro numero "))
+sum = n_1+n_2
+res = n_1-n_2
+divi = n_1/n_2
+mult = n_1*n_2
+print(f"La suma es {sum}\n La resta es {res}\n La division es {divi}\n La multiplicacion es {mult}\n ")
